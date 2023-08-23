c421ffc67f6989b1449d61f9200d0cefb333ce4b
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-08-23 11:14:36 +0530

    added cart slice in store

---
+++ src/Store/cart-slice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name:'cart',
+    initilState : {},
+    reducers:{
+
+    }
+})
+
+export const cartActions= cartSlice.actions;
+
+export default cartSlice;

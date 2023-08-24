dedfe071e1befb3ee2b8aaff51e0a94ad34c6b5a
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-08-24 10:45:52 +0530

    addtocart method added in store

--- src/Store/cart-slice.js
+++ src/Store/cart-slice.js
@@ -2,8 +2,32 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
     name:'cart',
-    initilState : {},
+    initilState : {
+        items :[],
+        totalQuantity : 0,
+       // totalAmount : 0,
+    },
     reducers:{
+        addItemToCart(state, action){
+            const newItem = action.payload;
+            const existingItem = state.items.find((item) => item.id ===newItem.id);
+            if(!existingItem) //if new item not present then add to the items array
+            {
+                state.items.push({
+                    itemId : newItem.id,
+                    price:newItem.price,
+                    quantity:1,
+                    totalPrice : newItem.price,
+                    name : newItem.title,
+                });
+            }
+            else{
+               existingItem.quantity++;
+               existingItem.totalPrice = existingItem.totalPrice + newItem.price; 
+            }
+        },
+        removeItemFromCart(){},
+        
 
     }
 })

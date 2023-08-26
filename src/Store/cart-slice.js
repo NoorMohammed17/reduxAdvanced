65b3541d2b882e523c81de0f1148ca4f8949b67f
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-08-26 22:14:50 +0530

    notification file added

--- src/Store/cart-slice.js
+++ src/Store/cart-slice.js
@@ -8,6 +8,10 @@ const cartSlice = createSlice({
     // totalAmount : 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
